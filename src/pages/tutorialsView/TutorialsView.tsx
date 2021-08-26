d1a7d8fa5333aec3b6af4e4a0a3c76f1039cb27a
agatalenz <lenz.agata@gmail.com>
2021-08-26 20:41:12 +0200

    added sample content

--- src/pages/tutorialsView/TutorialsView.tsx
+++ src/pages/tutorialsView/TutorialsView.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { get } from 'utils/web/webMethods/adapters';
 import { SectionsList } from './sectionsList';
 import { Section } from './sectionsList/common';
+import { SubsectionView } from './subsectionView';
 import './TutorialsView.scss';
 
 const TutorialsView = (): JSX.Element => {
@@ -21,7 +22,9 @@ const TutorialsView = (): JSX.Element => {
 					<SectionsList sections={sections} />
 				</div>
 			</div>
-			<div className="subsectionView">subsection View</div>
+			<div className="subsectionView">
+				<SubsectionView />
+			</div>
 		</div>
 	);
 };
